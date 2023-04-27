dbd92ce0a10956ea9ad26d7ed5933f0f3af2c489
bleo-m <bleo@mit.edu>
2023-04-27 01:59:42 -0400

    testing git username

--- src/stores.js
+++ src/stores.js
@@ -1,6 +1,7 @@
 import {persisted} from 'svelte-local-storage-store';
 export const animationDetails = persisted('animationDetails', {direction: 'Left'});
 
+// used for animation purposes
 export const oppositeDirections = {
     'Left': 'Right',
     'Right': 'Left',
