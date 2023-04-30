f420386855fdd61903260e74a1a4303cf04b60cf
bleo-m <bleo@mit.edu>
2023-04-30 19:51:27 -0400

    changing build

--- src/stores.js
+++ src/stores.js
@@ -5,7 +5,8 @@ import * as d3 from 'd3';
 export const animationDetails = persisted('animationDetails', {direction: 'Left'});
 
 // get data from remote csv file, but only fetch once instead on every page we need it
-const maintableData = await d3.csv('https://gist.githubusercontent.com/bleo-m/9c41a5d7fb245e84aafd2b3ba19c68b9/raw/6f74fd01b37abf8bfcc6866d5f6c8fa6815ebeb5/main_table.csv');
+let maintableData = [];
+d3.csv('https://gist.githubusercontent.com/bleo-m/9c41a5d7fb245e84aafd2b3ba19c68b9/raw/6f74fd01b37abf8bfcc6866d5f6c8fa6815ebeb5/main_table.csv').then((csv)=>{maintableData = csv;});
 export const mainTable = persisted('mainTable', maintableData);
 
 // used for animation purposes
