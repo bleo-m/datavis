271dd7e31848987e8b150cb6330de9046a38085e
bleo-m <bleo@mit.edu>
2023-04-30 19:47:10 -0400

    decreased data loading times

--- src/routes/migration/+page.js
+++ src/routes/migration/+page.js
@@ -1,12 +1,14 @@
 import * as d3 from 'd3';
-
+import { get } from 'svelte/store';
+import { mainTable } from '../../stores.js';
 /**
  * From the svelte documentation: 
  * Often, a page will need to load some data before it can be rendered. For this, we add a +page.js module that exports a load function:
  */
 
-// import data into page.svelte somehow. Svelte has a weird architecture because it has a different coupling between server and client.
+
+// get csv data from the store
 export async function load({ params }) {
-    const csvData = await d3.csv('https://gist.githubusercontent.com/bleo-m/9c41a5d7fb245e84aafd2b3ba19c68b9/raw/6f74fd01b37abf8bfcc6866d5f6c8fa6815ebeb5/main_table.csv');
+    let csvData = get(mainTable);  // get the maintable data from the store
     return {csv: csvData};
 }
