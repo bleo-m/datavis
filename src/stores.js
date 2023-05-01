22ac1641c20d5269c34b4660aa231ddff73896ea
bleo-m <bleo@mit.edu>
2023-04-30 20:04:25 -0400

    testing deployment

--- src/stores.js
+++ src/stores.js
@@ -4,10 +4,15 @@ import * as d3 from 'd3';
 // stores animationDirections
 export const animationDetails = persisted('animationDetails', {direction: 'Left'});
 
-// get data from remote csv file, but only fetch once instead on every page we need it
-let maintableData = [];
-d3.csv('https://gist.githubusercontent.com/bleo-m/9c41a5d7fb245e84aafd2b3ba19c68b9/raw/6f74fd01b37abf8bfcc6866d5f6c8fa6815ebeb5/main_table.csv').then((csv)=>{maintableData = csv;});
-export const mainTable = persisted('mainTable', maintableData);
+// using 'then' because deployment onto older browsers would not work with 'await'
+
+const getMainTableData = async () => {
+    // get data from remote csv file, but only fetch once instead on every page we need it
+    const data = await d3.csv('https://gist.githubusercontent.com/bleo-m/9c41a5d7fb245e84aafd2b3ba19c68b9/raw/6f74fd01b37abf8bfcc6866d5f6c8fa6815ebeb5/main_table.csv');
+    return data;
+}
+
+export const mainTable = persisted('mainTable', getMainTableData());
 
 // used for animation purposes
 export const oppositeDirections = {
