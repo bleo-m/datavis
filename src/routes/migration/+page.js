d7f8966146e2a47741ba068a8d83b5780e1789e3
bleo-m <bleo@mit.edu>
2023-05-03 01:24:48 -0400

    better data stuff

--- src/routes/migration/+page.js
+++ src/routes/migration/+page.js
@@ -1,15 +1,8 @@
 import * as d3 from 'd3';
-/**
- * From the svelte documentation: 
- * Often, a page will need to load some data before it can be rendered. For this, we add a +page.js module that exports a load function:
- */
-
 
 /**
  * Get csv data from parent file
  */
 export async function load({ parent }) {
-    const csvData = [];
     const {csv} = await parent();
-    return {data: csvData};
 }
