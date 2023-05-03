97ac5cc8558da3288488d2cf81b2a2fa70f77ecc
Erin <erinwzhang@gmail.com>
2023-05-03 00:29:04 -0400

    Add text to food insecurity page

--- src/routes/climate/+page.svelte
+++ src/routes/climate/+page.svelte
@@ -10,39 +10,39 @@
   navigationPage1="food" navigationDirection1="down"
   navigationPage2="" navigationDirection2="left" 
   title='CLIMATE CHANGE' slideNumber=1>
-  <div class="vis">
-    <p>
-    In recent years, climate change has greatly affected the countries in Central America's Northern Triangle - El Salvador, Guatemala, and Honduras, leading to food insecurity and 
-    migration. They are uniquely susceptible to the increase in greenhouse gas emissions, seen in figure [ ], despite them contributing near-nothing (less than 1%)  to climate change in 
-    comparison to the United States alone. This is due to the damages that changes in the climate have inflicted upon their agriculture, forestry, and fishing industries, upon which they 
-    rely heavily. Just as emissions have increased appallingly since 1990, the percentage of forest area has steadily decreased for each Northern Triangle country. And despite efforts to 
-    maintain the primary source of income for millions of people, as per the shaky increase in agricultural land, other factors have made it so that the profitability of rural industries 
-    has steeply dropped (figure [ ]).
-    </p>
-    <div class="chart">
-      <h3>Rainfall Anomalies</h3>
-      <LineGraph bind:data={data}/>
+    <div class="vis">
+      <p>
+      In recent years, climate change has greatly affected the countries in Central America's Northern Triangle - El Salvador, Guatemala, and Honduras, leading to food insecurity and 
+      migration. They are uniquely susceptible to the increase in greenhouse gas emissions, seen in figure [ ], despite them contributing near-nothing (less than 1%)  to climate change in 
+      comparison to the United States alone. This is due to the damages that changes in the climate have inflicted upon their agriculture, forestry, and fishing industries, upon which they 
+      rely heavily. Just as emissions have increased appallingly since 1990, the percentage of forest area has steadily decreased for each Northern Triangle country. And despite efforts to 
+      maintain the primary source of income for millions of people, as per the shaky increase in agricultural land, other factors have made it so that the profitability of rural industries 
+      has steeply dropped (figure [ ]).
+      </p>
+      <div class="chart">
+        <h3>Rainfall Anomalies</h3>
+        <LineGraph bind:data={data}/>
+      </div>
+      <p>
+      Among many others, anomalies in rainfall is one of these factors, as seen in figure [ ]. “According to the Intergovernmental Panel on Climate Change (IPCC), weather patterns are likely 
+      becoming more extreme and unpredictable, with more frequent droughts and floods that cause dangerous landslides, reduce agricultural yields, and destroy infrastructure. After an 
+      irregular rainy season in Guatemala, for instance, farmers lost almost 80 percent of corn grown in the highlands. In El Salvador, 50 percent of corn and bean farmers lost half of their 
+      crop after Tropical Storms Amanda and Cristóbal in May and June 2020.”
+      </p>
+      <p>
+      This constitutes a humanitarian crisis. Beyond the damages to the land and profits, climate change has caused more diseases to emerge. Rural populations are hit especially hard, as 
+      “rural communities often rely on subsistence agriculture and natural resources.” Changes and losses regarding agriculture can destroy their entire livelihood; as such, people in the 
+      Northern Triangle, especially those in rural areas “without employment, formal education, or a social safety net”, are forced to migrate externally in search of work. To this point, 
+      see the decline in rural populations by navigating to “Migration.” As this tends to be primarily men, families are also divided (see figure [ ]). <br><br>
+
+      And “toward the end of the century, as temperatures in Central America rise between 2.1°C and 3.3°C, the Northern Triangle is expected to experience increased aridity and drought, 
+      increased fire-prone weather conditions, and more extreme weather events. High temperatures result in increased evapotranspiration, reducing water availability, raising food prices, 
+      and creating food insecurity.”
+
+      To learn more about food insecurity, click down.
+      </p>
+
     </div>
-    <p>
-    Among many others, anomalies in rainfall is one of these factors, as seen in figure [ ]. “According to the Intergovernmental Panel on Climate Change (IPCC), weather patterns are likely 
-    becoming more extreme and unpredictable, with more frequent droughts and floods that cause dangerous landslides, reduce agricultural yields, and destroy infrastructure. After an 
-    irregular rainy season in Guatemala, for instance, farmers lost almost 80 percent of corn grown in the highlands. In El Salvador, 50 percent of corn and bean farmers lost half of their 
-    crop after Tropical Storms Amanda and Cristóbal in May and June 2020.”
-    </p>
-    <p>
-    This constitutes a humanitarian crisis. Beyond the damages to the land and profits, climate change has caused more diseases to emerge. Rural populations are hit especially hard, as 
-    “rural communities often rely on subsistence agriculture and natural resources.” Changes and losses regarding agriculture can destroy their entire livelihood; as such, people in the 
-    Northern Triangle, especially those in rural areas “without employment, formal education, or a social safety net”, are forced to migrate externally in search of work. To this point, 
-    see the decline in rural populations by navigating to “Migration.” As this tends to be primarily men, families are also divided (see figure [ ]). <br><br>
-
-    And “toward the end of the century, as temperatures in Central America rise between 2.1°C and 3.3°C, the Northern Triangle is expected to experience increased aridity and drought, 
-    increased fire-prone weather conditions, and more extreme weather events. High temperatures result in increased evapotranspiration, reducing water availability, raising food prices, 
-    and creating food insecurity.”
-
-    To learn more about food insecurity, click down.
-    </p>
-
-  </div>
   </PageSkeleton>
 </main>
 
--- src/routes/food/+page.svelte
+++ src/routes/food/+page.svelte
@@ -5,15 +5,51 @@
 
 <main>
   <PageSkeleton
-    title="FOOD INSECURITY"
-    navigationPage1="climate"
-    navigationPage2="migration"
-    navigationDirection1="up"
-    navigationDirection2="left"
-    slideNumber={3} 
-  />
+  title="FOOD INSECURITY"
+  navigationPage1="climate"
+  navigationPage2="migration"
+  navigationDirection1="up"
+  navigationDirection2="left"
+  slideNumber={3}>
+    <div class="vis">
+      <p>
+        Food insecurity is both a result of and a driving factor for migration, and is definitely exacerbated by climate change. It is substantially the result of lowered profits from 
+        agricultural production, and has drastically risen recently for the Northern Triangle. The number of people experiencing moderate or severe food insecurity has almost quadrupled from 
+        2019, from 4.8 million to 17.3 million by 2020.”
+      </p>
+      <p>
+        In data taken directly from survey responders in this region of Central America, people have to borrow money on a regular basis just to afford food (see figure [ ]). For the three 
+        countries – El Salvador, Guatemala, and Honduras – almost 7 million people experience acute hunger (Integrated Food Security Phase Classification (IPC) Phase 3+) as of 2022. As the 
+        country that has the most forest area and relies on agricultural profits the most, Honduras holds the highest percentage with 27% of its population experiencing acute hunger.
+      </p>
+      <p>
+        The World Food Programme (WFP) considers conflict, rainfall/vegetation deficits, and excess rainfall as the indicators for food insecurity in the breakdown of their key trends. The 
+        latter two are directly correlated to climate change, but the first is not necessarily exempt, as threats to livelihoods and disasters can easily yield higher tensions.
+      </p>
+      <p>
+        Migration is also heavily linked to food insecurity. According to a 2021 survey, “among people experiencing severe hunger, 37% said they had plans to migrate, with 23% having made 
+        concrete preparations to do so. By comparison, only 18% of people experiencing little to no hunger said they intended to migrate, with just 7% having prepared for the journey.” And, 
+        as WFP Executive David Beasley says, “When you can’t feed your children, desperation will force you on the road in search of food and safety.” Quantitative data says the same: as 
+        shown in figure [ ], migration has been consistently negative in the Northern Triangle.
+      </p>
+      <p>
+        To take an even deeper look at food insecurity, the impacts of malnutrition on health can be further explored. [DATA] or [DATA]. This is especially true for the rural population, which 
+        can be seen in the “Migration” tab.
+      </p>
+    </div>
+  </PageSkeleton>
 </main>
 
 <style>
+  .vis {
+    display: flex;
+    flex-direction: column;
+  }
 
+  .vis p {
+    font-size: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 300;
+    line-height: 1.5em;
+  }
 </style>
